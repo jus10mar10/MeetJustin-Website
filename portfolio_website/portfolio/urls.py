4bb0b61ba1c38eeeec37a76450a0b735d51aa32e
Justin <102390525+jus10mar10@users.noreply.github.com>
2023-08-15 20:00:01 +0000

    style changes

--- portfolio_website/portfolio/urls.py
+++ portfolio_website/portfolio/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.Home, name="Home"),
     path("Home", views.Home, name="Home"),
     path("About", views.About, name="About"),
-    path("Coming", views.Coming, name="ComingSoon")
+    path("Contact", views.Contact, name="Contact")
 ]
--- portfolio_website/portfolio/views.py
+++ portfolio_website/portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-pages = ['Home', 'About', 'Contact', 'Coming Soon']
+pages = ['Home', 'About', 'Contact']
 
 def Home(request):
     context = {
@@ -17,9 +17,9 @@ def Home(request):
 def About(request):
     current_page = 'About'
     other_pages = [page for page in pages if page != current_page]
-    return render(request, 'about.html', {"current_page":current_page, "other_pages":other_pages})
+    return render(request, 'coming_soon.html', {"current_page":current_page, "other_pages":other_pages})
 
-def Coming(request):
+def Contact(request):
     current_page = 'Coming Soon'
     other_pages = [page for page in pages if page != current_page]
     return render(request, 'coming_soon.html', {"current_page":current_page, "other_pages":other_pages})
