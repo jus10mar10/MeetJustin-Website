2146c56d12e747efad74fa0dc0b1322e1e3fd113
Justin <102390525+jus10mar10@users.noreply.github.com>
2023-08-16 22:46:35 -0400

    edit

--- portfolio_website/portfolio/models.py
+++ portfolio_website/portfolio/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
-#
-# class Home(models.Model):
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    content = RichTextField()
+    image = models.ImageField(upload_to='media/')
+    # Add other fields like URL, date, etc.
--- portfolio_website/portfolio/views.py
+++ portfolio_website/portfolio/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Project
 
 pages = ['Home', 'Portfolio', 'Resources']
 
@@ -22,9 +23,14 @@ def Home(request):
 def Portfolio(request):
     current_page = 'Portfolio'
     other_pages = [page for page in pages if page != current_page]
-    return render(request, 'detail.html', {"current_page":current_page, "other_pages":other_pages})
+    context = get_object_or_404(Project, pk=1)
+    return render(request, 'detail.html', {"current_page":current_page, "other_pages":other_pages, "context":context})
 
 def Resources(request):
     current_page = 'Resources'
     other_pages = [page for page in pages if page != current_page]
     return render(request, 'coming_soon.html', {"current_page":current_page, "other_pages":other_pages})
+
+def project_detail(request, project_id):
+    project = get_list_or_404(Project, pk=project_id)
+    return render(request, 'portfolio/project_detail.html', {'project': project})
