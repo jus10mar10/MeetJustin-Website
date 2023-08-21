e4f32a89dfa6607bc6955a0414c8bdae05f7532d
jus10mar10 <justmar00@yahoo.com>
2023-08-21 00:02:57 -0400

    combined port and resource into a gallery view

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-from django.utils import timezone
+from datetime import date
 
 class Project(models.Model):
-    date = models.DateField(default=timezone.now().date)
+    date = models.DateField(default=date.today)
+    title = models.CharField(max_length=35)
+    description = models.TextField(max_length=140)
+    content = RichTextField()
+    image = models.ImageField(upload_to='thumbnails/')
+
+class Resource(models.Model):
+    date = models.DateField(default=date.today)
     title = models.CharField(max_length=35)
     description = models.TextField(max_length=140)
     content = RichTextField()
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Project
+from .models import Resource
 
 pages = ['Home', 'Portfolio', 'Resources']
 
@@ -22,14 +23,17 @@ def Home(request):
 
 def Portfolio(request):
     current_page = 'Portfolio'
+    page_description = "Explore my portfolio to see the data-driven projects I've tackled and the solutions I've crafted."
     other_pages = [page for page in pages if page != current_page]
     projects = get_list_or_404(Project)
-    return render(request, 'portfolio.html', {"current_page":current_page, "other_pages":other_pages, "projects":projects})
+    return render(request, 'gallery.html', {"current_page":current_page, "other_pages":other_pages, "projects":projects, "page_description":page_description})
 
 def Resources(request):
     current_page = 'Resources'
+    page_description = "Some helpful resources to help you on your tech journey."
     other_pages = [page for page in pages if page != current_page]
-    return render(request, 'coming_soon.html', {"current_page":current_page, "other_pages":other_pages})
+    projects = get_list_or_404(Resource)
+    return render(request, 'resource.html', {"current_page":current_page, "other_pages":other_pages, "projects":projects})
 
 def project_detail(request, project_id):
     current_page = 'Portfolio'
