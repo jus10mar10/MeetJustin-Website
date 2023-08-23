371a12edd025097866620817d949381d4eee4dcb
Justin <102390525+jus10mar10@users.noreply.github.com>
2023-08-23 13:11:42 -0400

    title shown in admin view

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -16,3 +16,6 @@ class Post(models.Model):
     image = models.ImageField(upload_to='thumbnails/')
     type = models.CharField(max_length=20, choices=Choices.TYPE_CHOICES)
     is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
