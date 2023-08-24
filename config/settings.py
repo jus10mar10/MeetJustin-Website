6d32b91593305f54036d3a6769169c1372144e8d
Justin <102390525+jus10mar10@users.noreply.github.com>
2023-08-24 15:29:57 -0400

    settings update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='cc+rrq$!r7-y^u(yah07-*cwc@_m_csy(-udqka(++c-_tb#sp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
